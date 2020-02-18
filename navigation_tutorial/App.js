0fa36d5a8c774719afbad3961f63f63604cc9ec6
92pino <jjb8382@gmail.com>
2020-02-18 11:57:03 +0900

    :alembic: react-navigation tabbar

--- navigation_tutorial/App.js
+++ navigation_tutorial/App.js
@@ -1,84 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, StatusBar, Button, StyleSheet, SafeAreaView } from 'react-native';
+import React, { Component } from 'react';
+import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-// import { SafeAreaProvider } from 'react-native-safe-area-context';
-// import SafeAreaView from 'react-native-safe-area-view';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const HomeScreen = ({ navigation, route }) => {
-  useEffect(() => {
-    if (route.params?.post) {
-      console.log(1111);
-    }
-  }, [route.params?.post]);
+const Tab = createBottomTabNavigator();
+
+function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
       <Button
         title="Go to Details"
-        onPress={() => navigation.navigate('CreatePost')}
+        onPress={() => navigation.navigate('Details')}
       />
-      <Text style={{ margin: 10 }}>Post: {route.params?.post}</Text>
     </View>
-  )
+  );
 }
 
-const CreatePostScreen = ({ navigation, route }) => {
-  const [postText, setPostText] = useState('');
-
-  return (
-    <>
-      <TextInput
-        multiline
-        placeholder="What's on your mind?"
-        style={{ height: 200, padding: 10, backgroundColor: '#fff'}}
-        value={postText}
-        onChangeText={setPostText}
-      />
-      <Button
-        title="Done"
-        onPress={() => {
-          navigation.navigate('Home', { post: postText })
-        }}
-      />
-    </>
-  )
-}
-
-const DetailsScreen = ({ route, navigation }) => {
-  const { itemId } = route.params;
-  const { otherParam } = route.params;
+function DetailsScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
-      <Text>itemId: {JSON.stringify(itemId)}</Text>
-      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
-      <Button
-        title="Go to Details... again"
-        onPress={() => 
-          navigation.push('Details', {
-            itemId: Math.floor(Math.random() * 100),
-          })
-        }
-      />
-      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
       <Button title="Go back" onPress={() => navigation.goBack()} />
     </View>
-  )
+  );
 }
 
 const Stack = createStackNavigator();
 
-const App = () => {
+function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      {/* <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Details" component={DetailsScreen} />
-        <Stack.Screen name="CreatePost" component={CreatePostScreen} />
-      </Stack.Navigator>
+      </Stack.Navigator> */}
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Details" component={DetailsScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
-};
+}
 
 export default App;
