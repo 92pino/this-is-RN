587309f02c001aab02405d1108f21bf33642f7dd
92pino <jjb8382@gmail.com>
2020-03-20 19:00:53 +0900

    :ambulance:current value / total value percentage & VictoryPie Label font style add:ambulance:

--- PieChart/src/Donut.js
+++ PieChart/src/Donut.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import Svg from 'react-native-svg';
-import { VictoryPie, VictoryLabel } from "victory-native";
+import { VictoryPie, VictoryLabel } from 'victory-native';
 
 // x : label text, y : value값
 const data = [
@@ -12,25 +12,45 @@ const data = [
   { x: 3, y: 20},
   { x: 3, y: 5},
   { x: 3, y: 5},
+  { x: 3, y: 40},
 ];
 
-const size = 500;
+const size = 280;
+let sum = 0;
+
+// 배열 전체 값 합
+data.map(o => {
+  sum += o.y;
+});
+
+var total = sum - data[data.length - 1].y;
+// 전체 합 - 마지막 값 * 100 이후 소수점 버리기
+let result = Math.floor((total / sum) * 100);
 
 const Donut = () => {
   return (
     <View style={styles.container}>
-      {/* <VictoryPie data={data} x="quarter" y="earnings" /> */}
+      {/* <VictoryPie data={data} x='quarter' y='earnings' /> */}
       <Svg width={size} height={size}>
         <VictoryPie 
           standalone={false}
           width={size}
           height={size}
           data={data}       // 데이터
-          innerRadius={size / 4}  // 내부 공백 사이즈
+          innerRadius={size / 2}  // 내부 공백 사이즈
           labelRadius={0}   // 라벨 위치
-          colorScale={["red", "orange", "pink", "blue", "green", "purple", "white"]}  // 각 데이터별 color값
+          colorScale={[
+            'red',
+            'orange',
+            'pink',
+            'blue',
+            'green',
+            'purple',
+            'white',
+            'gray',
+          ]} // 각 데이터별 color값
           style={{
-            labels: { display: 'none' }
+            labels: {display: 'none'},
           }}
         />
         <VictoryLabel
@@ -39,22 +59,21 @@ const Donut = () => {
           style={[
             // 배열로 각각 스타일을 줄수 있다.
             {
-              fontSize: 30,
-              color: 'red',
-              fontWeight: "600",
-              lineHeight: 40
+              fontSize: 20,
+              fontWeight: '600',  // 폰트 두께
+              fill: 'green',      // 글자 색상
             },
             {
-              fontSize: 30,
+              fontSize: 40,
               color: 'red',
-              fontWeight: "bold",
-              lineHeight: 40
-            }
+              fontWeight: 'bold',
+            },
           ]}
+          lineHeight={[1.5, 1.5]}
           // label 중앙 정렬
           x={size * 0.5}
           y={size * 0.5}
-          text={["2,717.3", "lbs of credit", `${data[0].y + data[1].y + data[2].y}`]}
+          text={['총 예산 대비', `${result}%`]}
         />
       </Svg>
     </View>
@@ -64,10 +83,10 @@ const Donut = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#f5fcff"
-  }
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5fcff',
+  },
 });
 
 export default Donut;
